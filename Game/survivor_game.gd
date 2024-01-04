413cf3536a606e863392408d98ec59f6a15a4f32
Daniel Schwettman <48576763+daniel-schwettman@users.noreply.github.com>
2024-01-04 12:04:40 -0500

    Folder Restructure
    -Clean up folder structure for a cleaner project
    
    -Change briefcase rotation to = ShootingPoint.rotation instead of player.rotation

---
+++ Game/survivor_game.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+@onready var upgradeLabel = get_node("/root/Game/UpgradeStation/UpgradeLabel")
+const PINE_TREE = preload("res://Trees/pine_tree.tscn")
+var maxTreeCount = 25
+var numberofLevels
+
+func _ready():
+	BackGroundMusic.play()
+	get_tree().paused = true
+	#createTrees()
+
+func createTrees():
+	for i in range(10):
+		var new_tree = PINE_TREE.instantiate()
+		new_tree.add_to_group("Trees")
+		new_tree.global_position.x = randf_range(0, 3400)
+		new_tree.global_position.y = randf_range(0, 3400)
+		add_child(new_tree)
+
+func _process(_delta):
+	var playerMoney = "%.2f" % %Player.money
+	%MoneyCount.text = "Money: $" + str(playerMoney)
+	if $Player.canPurchase:
+		upgradeLabel.modulate = Color("00FF00")
+		$Player.canPurchase = true
+	else:
+		$Player.canPurchase = false
+		upgradeLabel.modulate = Color("FF0000")
+	
+	if !BackGroundMusic.playing:
+		BackGroundMusic.play()
+
+func _on_player_health_depleted():
+	%GameOver.visible = true
+	BackGroundMusic.stop()
+	MenuMusic.play()
+	get_tree().paused = true;
+
+
+func _on_briefcase_health_depleted():
+	%Label.text = "You let them destroy the briefcase..."
+	%GameOver.visible = true
+	BackGroundMusic.stop()
+	MenuMusic.play()
+	get_tree().paused = true;
---
+++ Weapons/gun.gd
@@ -0,0 +1,46 @@
+extends Area2D
+
+@onready var player = get_node("/root/Game/Player")
+@onready var briefcase = get_node("/root/Game/Briefcase")
+var isShooting = false
+var frameCount = 30
+var isThrowing = false
+
+func _physics_process(_delta):
+	look_at(get_global_mouse_position())
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		isShooting = true
+	else:
+		isShooting = false
+	
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		isThrowing = true
+	else:
+		isThrowing = false
+	
+	frameCount += 1
+
+func shoot():
+	if frameCount >= player.frameCount:
+		const BULLET = preload("res://Weapons/bullet.tscn")
+		var new_bullet = BULLET.instantiate()
+		new_bullet.global_position = %ShootingPoint.global_position
+		new_bullet.global_rotation = %ShootingPoint.global_rotation
+		%ShootingPoint.add_child(new_bullet)
+		GunshotSfx.play()
+		frameCount = 0
+
+func throw():
+	if frameCount >= player.frameCount and briefcase.pickedUp:
+		briefcase.global_position = %ShootingPoint.global_position
+		briefcase.global_rotation = %ShootingPoint.global_rotation
+		briefcase.thrown = true
+		briefcase.inRange = false
+		GunshotSfx.play()
+		frameCount = 0
+		
+func _on_timer_timeout():
+	if isShooting:
+		shoot()
+	if isThrowing:
+		throw()
